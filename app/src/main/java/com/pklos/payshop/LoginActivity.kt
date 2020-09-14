3343002a962067d019bea31bcc41cf030dd91042
Przemyslaw Klos <pklos1992@gmail.com>
2020-09-14 19:32:43 +0200

    sign in based on Firebase added, lateinit vars added

--- app/src/main/java/com/pklos/payshop/LoginActivity.kt
+++ app/src/main/java/com/pklos/payshop/LoginActivity.kt
@@ -5,22 +5,27 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.FirebaseAuth
 import org.w3c.dom.Text
 
 class LoginActivity : AppCompatActivity() {
     private val TAG : String = "LoginActivity"
     private val REQUEST_SIGNUP : Int = 0
 
-    private var _emailTxt : EditText? = null
-    private var _passwordTxt : EditText? = null
-    private var _loginBtn : Button? = null
-    private var _signup_link : TextView? = null
+    private lateinit var _emailTxt : EditText
+    private lateinit var _passwordTxt : EditText
+    private lateinit var _loginBtn : Button
+    private lateinit var _signup_link : TextView
 
     private val handler = Handler()
+
+    private lateinit var mAuth : FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,15 +36,17 @@ class LoginActivity : AppCompatActivity() {
         _loginBtn = findViewById(R.id.login_btn)
         _signup_link = findViewById(R.id.signup_link)
 
-        _loginBtn?.setOnClickListener{
+        _loginBtn.setOnClickListener{
             login()
         }
 
-        _signup_link?.setOnClickListener {
+        _signup_link.setOnClickListener {
             //sign up activity start
             val intent = Intent(applicationContext, SignupActivity::class.java)
             startActivityForResult(intent, REQUEST_SIGNUP)
         }
+
+        mAuth = FirebaseAuth.getInstance()
     }
 
     private fun login(){
@@ -48,69 +55,69 @@ class LoginActivity : AppCompatActivity() {
             return
         }
 
-        _loginBtn?.isEnabled = false
+        _loginBtn.isEnabled = false
 
         val progressDialog = AppUtils.setProgressDialog(this, "Authentication in progress...")
         progressDialog.show()
 
-        val email : String = _emailTxt?.text.toString()
-        val password : String = _passwordTxt?.text.toString()
+        val email : String = _emailTxt.text.toString()
+        val password : String = _passwordTxt.text.toString()
 
         //authentication logic here
-        if (authenticate(email, password)){
-            handler.postDelayed({
-                onLoginSuccess()
-                progressDialog.dismiss()
-            }, 3000)
-        }
-        else{
-            handler.postDelayed({
-                onLoginFailed()
-                progressDialog.dismiss()
-            }, 3000)
-        }
+
+        mAuth.signInWithEmailAndPassword(email, password)
+             .addOnCompleteListener(this, OnCompleteListener {
+                 task ->
+                 if(task.isSuccessful){
+                     Log.d(TAG, "signInWithEmailAndPassword: success")
+                     Toast.makeText(this, "Successfully Logged", Toast.LENGTH_LONG).show()
+                     handler.postDelayed({
+                         onLoginSuccess()
+                         progressDialog.dismiss()
+                     }, 2000)
+                 }else {
+                     Log.d(TAG, "signInWithEmailAndPassword: failure")
+                     Toast.makeText(this, "Login Failed", Toast.LENGTH_LONG).show()
+                     handler.postDelayed({
+                         onLoginFailed()
+                         progressDialog.dismiss()
+                     }, 2000)
+                 }
+             })
     }
 
     private fun onLoginSuccess(){
-        _loginBtn?.isEnabled = true
+        _loginBtn.isEnabled = true
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
         finish()
     }
 
     private fun onLoginFailed(){
-        Toast.makeText(baseContext, "Login Failed!", Toast.LENGTH_LONG).show()
-        _loginBtn?.isEnabled = true
+        _loginBtn.isEnabled = true
     }
 
     private fun validateLoginData(): Boolean {
         var valid = true
 
-        val email : String = _emailTxt?.text.toString()
-        val password : String = _passwordTxt?.text.toString()
+        val email : String = _emailTxt.text.toString()
+        val password : String = _passwordTxt.text.toString()
 
         if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()){
-            _emailTxt?.error = "Enter Valid Email Address"
+            _emailTxt.error = "Enter Valid Email Address"
             valid = false
         } else{
-            _emailTxt?.error = null
+            _emailTxt.error = null
         }
 
         if (password.isEmpty() || password.length < 4){
-            _passwordTxt?.error = "Password Length Below 4 Characters"
+            _passwordTxt.error = "Password Length Below 4 Characters"
             valid = false
         } else{
-            _emailTxt?.error = null
+            _emailTxt.error = null
         }
 
         return valid
-    }
-
-    private fun authenticate(email : String, password : String) : Boolean{
-        //simple hardcoded authentication, for test
-//        if (email.equals("xxx@xxx.com") && password.equals("xxx")){
-//            return true
-//        }
-//
-        return false
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
