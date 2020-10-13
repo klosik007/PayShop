d26e5d06aa210d5028880878f0a7f35e343bb31b
Przemyslaw Klos <pklos1992@gmail.com>
2020-10-13 22:02:16 +0200

    mainactivity moved to activities folder

---
+++ app/src/main/java/com/pklos/payshop/activities/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.pklos.payshop.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.pklos.payshop.R
+import com.pklos.payshop.fragments.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val bottomNavView: BottomNavigationView = findViewById(R.id.bottom_nav_view)
+        bottomNavView.setOnNavigationItemSelectedListener(bottomNavListener)
+    }
+
+    private val bottomNavListener: BottomNavigationView.OnNavigationItemSelectedListener =
+        BottomNavigationView.OnNavigationItemSelectedListener {
+            lateinit var selectedFragment: Fragment
+            when(it.itemId){
+                R.id.nav_home -> selectedFragment = HomeFragment()
+                R.id.nav_search -> selectedFragment = SearchFragment()
+                R.id.nav_favorites -> selectedFragment = FavoritesFragment()
+                R.id.nav_basket -> selectedFragment = BasketFragment()
+                R.id.nav_profile -> selectedFragment = ProfileFragment()
+            }
+
+            supportFragmentManager.beginTransaction().replace(R.id.view_pager, selectedFragment).commit()
+            true
+        }
+}
