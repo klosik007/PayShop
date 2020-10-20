5542a837e2e44e9eaeab8bdca96b0f7418069592
Przemyslaw Klos <pklos1992@gmail.com>
2020-10-20 22:23:06 +0200

    fragment override methods added

--- app/src/main/java/com/pklos/payshop/fragments/HomeFragment.kt
+++ app/src/main/java/com/pklos/payshop/fragments/HomeFragment.kt
@@ -1,7 +1,30 @@
 package com.pklos.payshop.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.pklos.payshop.R
+import com.pklos.payshop.utils.AppUtils
 
 class HomeFragment: Fragment() {
+    private lateinit var welcomeTextView: TextView
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        //super.onViewCreated(view, savedInstanceState)
+        val dayTime = AppUtils.printWelcomeMessageOnDayTime()
+
+        welcomeTextView = view.findViewById(R.id.welcomeTextView)
+        welcomeTextView.text = "${getString(R.string.good)} $dayTime"
+    }
 }
--- app/src/main/java/com/pklos/payshop/fragments/SearchFragment.kt
+++ app/src/main/java/com/pklos/payshop/fragments/SearchFragment.kt
@@ -1,7 +1,23 @@
 package com.pklos.payshop.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.pklos.payshop.R
+import com.pklos.payshop.utils.AppUtils
 
 class SearchFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_search, container, false)
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        //super.onViewCreated(view, savedInstanceState)
+    }
 }
