36d20bdb73824efc75c1d788def034a2ac13b02a
Przemyslaw Klos <pklos1992@gmail.com>
2020-12-21 22:58:28 +0100

    price, category and isFavorite: bug fixes

--- app/src/main/java/com/pklos/payshop/data/ItemsDataClass.kt
+++ app/src/main/java/com/pklos/payshop/data/ItemsDataClass.kt
@@ -14,49 +14,6 @@ data class Item(val id: Int, val name: String, val category: Category, val price
     }
 }
 
-object ExampleData{
-    private val bread = Item(
-        id = 0,
-        name = "Bread",
-        category = Category.FOOD,
-        price = 2.50f,
-        isFavorite = false
-    )
-    private val ball = Item(
-        id = 1,
-        name = "Ball",
-        category = Category.SPORT,
-        price = 50.0f,
-        isFavorite = false
-    )
-    private val table = Item(
-        id = 2,
-        name = "Table",
-        category = Category.HOME,
-        price = 200.0f,
-        isFavorite = false
-    )
-    private val bike = Item(
-        id = 3,
-        name = "Bike",
-        category = Category.SPORT,
-        price = 1500.0f,
-        isFavorite = true
-    )
-
-    val dataList = listOf(bread, ball, table, bike)
-
-    /*fun getItem(id: Int): Item? {
-        for (item in dataList){
-            if (item.id == id){
-                return item
-            }
-        }
-
-        return null
-    }*/
-}
-
 object FirebaseData{
     private var db = Firebase.firestore
 
@@ -68,9 +25,9 @@ object FirebaseData{
                 val dataList = mutableListOf<Item>()
                 var id = 0
                 for (item in docs){
-                    val category = item.data.toString().removePrefix("{").removeSuffix("}").split(", ")[0].substringAfter("\"").removeSuffix("\"")
-                    val isFavorite = item.data.toString().removePrefix("{").removeSuffix("}").split(", ")[1].substringAfter("=").toBoolean()
-                    val price = item.data.toString().removePrefix("{").removeSuffix("}").split(", ")[2].substringAfter("=").toFloat()
+                    val price = item.data.toString().removePrefix("{").removeSuffix("}").split(", ")[0].substringAfter("=").toFloat()
+                    val category = item.data.toString().removePrefix("{").removeSuffix("}").split(", ")[1].substringAfter("=")
+                    val isFavorite = item.data.toString().removePrefix("{").removeSuffix("}").split(", ")[2].substringAfter("=").toBoolean()
                     dataItem = Item(
                         id = id,
                         name = item.id,
