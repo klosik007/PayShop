267f903838d5483870e7cdc71fcb3d67653fdb94
Przemo <pklos1992@gmail.com>
2020-12-29 23:42:42 +0100

    fun firebaseUpdateFavoriteStatus - updating isFavorite value in DB added

--- app/src/main/java/com/pklos/payshop/data/ItemsDataClass.kt
+++ app/src/main/java/com/pklos/payshop/data/ItemsDataClass.kt
@@ -16,7 +16,6 @@ data class Item(val id: Int, val name: String, val category: Category, val price
 
 object FirebaseData{
     private var db = Firebase.firestore
-
     private lateinit var dataItem: Item
 
     fun firebaseDataDownload(clb: MyCallback){
@@ -55,5 +54,20 @@ object FirebaseData{
             .addOnFailureListener { exception ->
                 Log.w("docs", "Error getting documents: ", exception)
             }
+    }
+
+    fun firebaseUpdateFavoriteStatus(docToUpdate: String/*, newValue: Boolean*/){
+        val sfDocRef = db.collection("items").document(docToUpdate)
+
+        db.runTransaction { transaction ->
+            val snapshot = transaction.get(sfDocRef)
+
+            val newFavoriteStatus = !(snapshot.getBoolean("isFavorite")!!)
+            transaction.update(sfDocRef, "isFavorite", newFavoriteStatus)
+            //transaction.update(sfDocRef, "isFavorite", newValue)
+
+            null
+        }.addOnSuccessListener { Log.d("updateData", "Transaction success!") }
+            .addOnFailureListener { e -> Log.w("updateData", "Transaction failure.", e) }
     }
 }
