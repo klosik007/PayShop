a1d4b76a5275e682ebcb04b761c19023fe63e107
klosik007 <pklos1992@gmail.com>
2021-04-18 23:42:13 +0200

    category and price handlers added

--- app/src/main/java/com/pklos/payshop/fragments/FiltersDialogWindowFragment.kt
+++ app/src/main/java/com/pklos/payshop/fragments/FiltersDialogWindowFragment.kt
@@ -5,15 +5,19 @@ import android.app.Dialog
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
 import android.widget.Toast
 import androidx.fragment.app.DialogFragment
 import com.pklos.payshop.R
+import com.pklos.payshop.data.Category
+import com.pklos.payshop.data.FirebaseData
+import com.pklos.payshop.data.Item
 import com.pklos.payshop.utils.DialogInterfaceListener
 import kotlinx.android.synthetic.*
 import java.lang.IllegalStateException
 
 class FiltersDialogWindowFragment(var listner: DialogInterfaceListener) : DialogFragment() {
-
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog{
         return activity?.let {
             val builder = AlertDialog.Builder(it)
@@ -48,6 +52,35 @@ class FiltersDialogWindowFragment(var listner: DialogInterfaceListener) : Dialog
             val sortNameAscending: Button = view.findViewById(R.id.sortBy_nameAscending)
             sortNameAscending.setOnClickListener {
                 listner.firebaseSortNameAscending()
+            }
+
+            val sportCategoryCheckbox: CheckBox = view.findViewById(R.id.sportCategory)
+            sportCategoryCheckbox.setOnCheckedChangeListener { _, isChecked ->
+                if(isChecked){
+                    FirebaseData.checkboxFilters.add { filter -> filter.category == Category.SPORT}
+                }else FirebaseData.checkboxFilters.remove { filter -> filter.category == Category.SPORT }
+            }
+
+            val homeCategoryCheckbox: CheckBox = view.findViewById(R.id.homeCategory)
+            homeCategoryCheckbox.setOnCheckedChangeListener { _, isChecked ->
+                if(isChecked){
+                    FirebaseData.checkboxFilters.add { filter -> filter.category == Category.HOME}
+                }else FirebaseData.checkboxFilters.remove { filter -> filter.category == Category.HOME }
+            }
+
+            val foodCategoryCheckbox: CheckBox = view.findViewById(R.id.foodCategory)
+            foodCategoryCheckbox.setOnCheckedChangeListener { _, isChecked ->
+                if(isChecked){
+                    FirebaseData.checkboxFilters.add { filter -> filter.category == Category.FOOD}
+                }else FirebaseData.checkboxFilters.remove { filter -> filter.category == Category.FOOD }
+            }
+
+            val editTextPriceFrom: EditText = view.findViewById(R.id.priceFromEditText)
+            //FirebaseData.priceFrom = editTextPriceFrom.text
+
+            val applyFiltersBtn: Button = view.findViewById(R.id.sortApplyBtn)
+            applyFiltersBtn.setOnClickListener {
+                listner.firebaseApplyFilters()
             }
 
             builder.setView(view)
