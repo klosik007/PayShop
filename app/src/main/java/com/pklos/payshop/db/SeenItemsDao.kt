325a25f6ebf7f5c4bac9d64880a9af204a2a715c
klosik007 <pklos1992@gmail.com>
2021-05-19 22:58:33 +0200

    SeenItemsDao sketch

---
+++ app/src/main/java/com/pklos/payshop/db/SeenItemsDao.kt
@@ -0,0 +1,18 @@
+package com.pklos.payshop.db
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface SeenItemsDao {
+    @Query("SELECT * FROM seenitems")
+    fun getAllSeenItems() : List<SeenItems>
+
+    @Insert
+    fun insertSeenItem(vararg seenItems: SeenItems)
+
+    @Delete
+    fun delete(seenItem: SeenItems)
+}
