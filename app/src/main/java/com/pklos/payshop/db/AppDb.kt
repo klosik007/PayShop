07c8b2e188da709337520eb560d41303e4d0abaa
klosik007 <pklos1992@gmail.com>
2021-06-11 00:03:36 +0200

    SeenItems db creation added

--- app/src/main/java/com/pklos/payshop/db/AppDb.kt
+++ app/src/main/java/com/pklos/payshop/db/AppDb.kt
@@ -1,9 +1,24 @@
 package com.pklos.payshop.db
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [SeenItems::class], version = 1)
 abstract class AppDb: RoomDatabase() {
     abstract fun seenItemsDao(): SeenItemsDao
+
+    companion object{
+        @Volatile private var instance: AppDb? = null
+        private val LOCK = Any()
+
+        operator fun invoke(context: Context) = instance ?: synchronized(LOCK){
+            instance ?: buildDb(context).also{instance = it}
+        }
+
+        private fun buildDb(context: Context) = Room.databaseBuilder(context,
+                                                AppDb::class.java, "seen-items.db")
+                                                .build()
+    }
 }
