decfc7969ace6f56ac736679ce7ed577f47ccf64
klosik007 <pklos1992@gmail.com>
2021-06-10 23:39:01 +0200

    @Entity annotation added

--- app/src/main/java/com/pklos/payshop/db/SeenItems.kt
+++ app/src/main/java/com/pklos/payshop/db/SeenItems.kt
@@ -1,8 +1,10 @@
 package com.pklos.payshop.db
 
 import androidx.room.ColumnInfo
+import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+@Entity(tableName = "seen_items")
 data class SeenItems(
     @PrimaryKey val id: Int,
     @ColumnInfo(name="item_id") val itemId: Int?
